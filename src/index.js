fed1e9ef2bb96fcaaca30c2eb110b497851d8e7b
Kevin Thomas <kevin.thomas@theculturetrip.com>
2020-01-15 14:14:54 +0000

    fixed redndering

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Gallery } from './Gallery';
-import { SubscriptionForm } from './SubscriptionForm';
 
-ReactDOM.render(window.location.pathname === '/gallery' ?  <Gallery/> : <SubscriptionForm/>, document.getElementById('root'));
+ReactDOM.render(window.location.pathname === '/gallery' ?  <Gallery/> : <div/>, document.getElementById('root'));
